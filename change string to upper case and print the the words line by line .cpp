8e78f325e09c761da7e4bbef4d11fe3e54adc4ff
Md.Sakib Chowdhury <136784151+csakib049@users.noreply.github.com>
2023-10-26 08:38:15 +0600

    Create change string to upper case and print the the words line by line .cpp

---
+++ change string to upper case and print the the words line by line .cpp
@@ -0,0 +1,37 @@
+/*
+
+input: sakib is a good boy
+
+output: SAKIB IS A GOOD BOY
+
+*/
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+char upper(char c ){
+  return 'A'+(c-'a');
+}
+
+int main(){
+
+while(true){
+    string s ;
+    cin>>s;
+    if(s.size()==0){
+
+        break;
+    }
+    for(int i=0;i<s.size();i++){
+
+        s[i]=upper(s[i]);
+
+    }
+
+
+    cout<<s<<endl;
+
+}
+
+}
