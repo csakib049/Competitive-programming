d6d6acb2eecdf43735fa5c1646dd39701a678075
Md.Sakib Chowdhury <136784151+csakib049@users.noreply.github.com>
2023-11-01 21:20:58 +0600

    Create A vertex is considered as a point with both its coordinates as integers.cpp

---
+++ A vertex is considered as a point with both its coordinates as integers.cpp
@@ -0,0 +1,25 @@
+/*
+Sample Input:
+2
+3
+1
+
+Sample Output:
+12
+4
+
+https://www.hackerearth.com/problem/algorithm/city-tour/
+
+*/
+#include<bits/stdc++.h>
+using namespace std;
+  int main(){
+      int t;
+      cin>>t;
+      
+      while(t--){
+          long long n;
+          cin>>n;
+          cout<<n*4<<endl;
+      }
+  }
