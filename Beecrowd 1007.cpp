693b1c1dd5b9726595a5f6a0741a121949df8f81
Md.Sakib Chowdhury <136784151+csakib049@users.noreply.github.com>
2023-12-29 18:38:43 +0600

    Beecrowd 1007.cpp

---
+++ Beecrowd 1007.cpp
@@ -0,0 +1,14 @@
+//https://www.beecrowd.com.br/judge/en/problems/view/1007
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    
+    int a,b,c,d,DIFERENCA=0;
+    
+    cin>>a>>b>>c>>d;
+    DIFERENCA =(a * b - c * d);
+    
+    cout<<"DIFERENCA = "<<DIFERENCA<<endl;
+
+}
