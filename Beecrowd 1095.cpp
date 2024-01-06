81bb53fbd0fd8e8705e1de145a541873e34e06bc
Md.Sakib Chowdhury <136784151+csakib049@users.noreply.github.com>
2024-01-06 07:51:29 +0600

    Create Beecrowd 1095.cpp

---
+++ Beecrowd 1095.cpp
@@ -0,0 +1,15 @@
+//https://www.beecrowd.com.br/judge/en/problems/view/1095
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int i,j=60;
+
+    for( i=1; j>=0; i+=3)
+    {
+    cout<<"I="<<i<<" J="<<j<<endl;
+        j=j-5;
+    }
+
+}
