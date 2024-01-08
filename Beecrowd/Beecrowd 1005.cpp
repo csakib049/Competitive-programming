43c40fe7f00ee229f142aa3978c1b3bab2888a1d
Md.Sakib Chowdhury <136784151+csakib049@users.noreply.github.com>
2024-01-08 13:55:40 +0600

    Add files via upload

---
+++ Beecrowd/Beecrowd 1005.cpp
@@ -0,0 +1,20 @@
+//https://www.beecrowd.com.br/judge/en/problems/view/1005
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    
+   double a,b,MEDIA=0;
+    
+    cin>>a>>b;
+    
+    MEDIA=((a*3.5)+(b*7.5))/(3.5+7.5);
+    
+    cout<<"MEDIA = "<<fixed<<setprecision(5)<<MEDIA<<endl;
+  
+}
+
+
+
+
