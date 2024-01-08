97a0b294a7e2a0a7a2cc6d6681b58e5ff2a3f5c0
Md.Sakib Chowdhury <136784151+csakib049@users.noreply.github.com>
2024-01-08 14:02:40 +0600

    Add files via upload

---
+++ Beecrowd/Beecrowd 1014.cpp
@@ -0,0 +1,14 @@
+//https://www.beecrowd.com.br/judge/en/problems/view/1014
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+   float a,b;
+   
+   cin>>a>>b;
+   
+   cout<<fixed<<setprecision(3)<<a/b<<" km/l"<<endl;
+  
+    return 0;
+}
