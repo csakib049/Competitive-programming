1fbfe468680a76900e7ca3623fb09afad4432ba8
Md.Sakib Chowdhury <136784151+csakib049@users.noreply.github.com>
2024-01-08 23:53:28 +0600

    Create Beecrowd 1155.cpp

---
+++ Beecrowd/Beecrowd 1155.cpp
@@ -0,0 +1,20 @@
+
+//https://www.beecrowd.com.br/judge/en/problems/view/1155
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+   double  s=0,i,c=0;
+    for( i=1;i<=100;i++){
+        
+        c=(1/i);
+       s=s+c;
+     
+        
+    }
+    
+  cout<<fixed<<setprecision(2)<<s<<endl;
+
+}
