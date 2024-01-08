6082380230df64d08f8835e353db2e88b3669e41
Md.Sakib Chowdhury <136784151+csakib049@users.noreply.github.com>
2024-01-08 14:03:58 +0600

    Add files via upload

---
+++ Beecrowd/Beecrowd 1017.cpp
@@ -0,0 +1,14 @@
+//https://www.beecrowd.com.br/judge/en/problems/view/1017
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    
+    float a,b;
+    cin>>a>>b;
+    
+    cout<<fixed<<setprecision(3)<<(a*b)/12<<endl;
+    
+    
+}
