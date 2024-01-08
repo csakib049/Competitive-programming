9e896df5e6da4d3c0b74c329da31f76389c9e6b4
Md.Sakib Chowdhury <136784151+csakib049@users.noreply.github.com>
2024-01-08 14:00:26 +0600

    Add files via upload

---
+++ Beecrowd/Beecrowd 1008.cpp
@@ -0,0 +1,17 @@
+//https://www.beecrowd.com.br/judge/en/problems/view/1008
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+    int a;
+    float b,c;
+    
+    cin>>a>>b>>c;
+    
+    cout<<"NUMBER = "<<a<<endl;
+    cout<<"SALARY = U$ "<<fixed<<setprecision(2)<<b*c<<endl;
+    
+    
+}
