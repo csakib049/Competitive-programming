d27453ab630ef30cf778b17609e095280d80e555
Md.Sakib Chowdhury <136784151+csakib049@users.noreply.github.com>
2024-01-12 16:16:55 +0600

    Create Beecrowd 1177.cpp

---
+++ Beecrowd/Beecrowd 1177.cpp
@@ -0,0 +1,21 @@
+//https://www.beecrowd.com.br/judge/en/problems/view/1177
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    long a[1000],b,c;
+
+    cin>>b;
+
+    for(int i=0,c=0;i<1000;i++){
+        cout<<"N["<<i<<"] = "<< c++ <<endl;
+
+          if(c==b){
+            c=0;
+          }
+
+        }
+    }
