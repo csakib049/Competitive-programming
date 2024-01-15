dc2d44669cb0817aed48b173c16adf88cfc35f68
Md.Sakib Chowdhury <136784151+csakib049@users.noreply.github.com>
2024-01-15 19:21:14 +0600

    Create A. Odd One Out.cpp

---
+++ Code Forces/A. Odd One Out.cpp
@@ -0,0 +1,24 @@
+//https://codeforces.com/contest/1915/problem/A
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+int t;
+cin>>t;
+while(t--){
+        int a,b,c;
+
+    cin>>a>>b>>c;
+    if(a==b){
+        cout<<c<<endl;
+    }else if(b==c){
+
+        cout<<a<<endl;
+    }else if(c==a){
+
+        cout<<b<<endl;
+    }
+  }
+}
