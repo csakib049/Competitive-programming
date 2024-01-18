3610376b3d81865cbb56c36665da56312d297ecc
Md.Sakib Chowdhury <136784151+csakib049@users.noreply.github.com>
2024-01-18 23:28:23 +0600

    Create Codeforces Checking.cpp

---
+++ Code Forces/Codeforces Checking.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+
+    while(t--)
+    {
+
+        char x;
+        cin>>x;
+
+        if(x=='c' || x=='o' || x=='d' || x=='e' || x=='f' || x=='o' || x=='r' || x=='c' || x=='e' || x=='s')
+        {
+
+            cout<<"YES"<<endl;
+
+        }
+        else
+        {
+
+            cout<<"NO"<<endl;
+
+        }
+
+    }
+
+}
