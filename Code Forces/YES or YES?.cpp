5e7de876052538f294fcbce587b40ec1c029f659
Md.Sakib Chowdhury <136784151+csakib049@users.noreply.github.com>
2024-01-19 08:48:50 +0600

    Create YES or YES?.cpp

---
+++ Code Forces/YES or YES?.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+int t;
+cin>>t;
+while(t--){
+    string s;
+    cin>>s;
+
+    if(s=="YES" || s=="yes" ||  s=="Yes" || s=="yEs" || s=="yeS" || s=="YeS" || s=="yES" || s=="YEs" ){
+        cout<<"YES"<<endl;
+    }else {
+        cout<<"NO"<<endl;
+    }
+}
+}
