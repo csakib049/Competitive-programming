2831ec863eb0068506e31e263fdd6397e5258632
Md.Sakib Chowdhury <136784151+csakib049@users.noreply.github.com>
2024-02-13 20:19:08 +0600

    Create Counting Divisors.cpp

---
+++ CSES/Counting Divisors.cpp
@@ -0,0 +1,31 @@
+//================================================================================\\
+//===========================( اللهم صّلِ وسَلّمْ عَلۓِ نَبِيْنَا مُحَمد ﷺ )=========================\\
+//================================================================================\\
+ 
+ 
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#define endl "\n"
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define fraction() cout.unsetf(ios::floatfield); cout.precision(10); cout.setf(ios::fixed,ios::floatfield);
+ 
+const int maxX = 1e6;
+ 
+int N, x, d[maxX+1];
+ 
+void init(){
+    for(int i = 1; i <= maxX; i++)
+        for(int j = i; j <= maxX; j += i)
+            d[j]++;
+}
+ 
+int main(){
+    scanf("%d", &N);
+ 
+    init();
+    for(int i = 0; i < N; i++){
+        scanf("%d", &x);
+        printf("%d\n", d[x]);
+    }
+}
