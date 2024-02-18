c0222672cdbb811b1eff45a588a5de09782d2597
Md.Sakib Chowdhury <136784151+csakib049@users.noreply.github.com>
2024-02-18 20:24:14 +0600

    Create Prime Deletion.cpp

---
+++ Code Forces/Prime Deletion.cpp
@@ -0,0 +1,39 @@
+//https://codeforces.com/problemset/problem/1861/A
+
+//================================================================================\\
+//===========================( اللهم صّلِ وسَلّمْ عَلۓِ نَبِيْنَا مُحَمد ﷺ )=========================\\
+//================================================================================\\
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define endl "\n"
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define fraction() cout.unsetf(ios::floatfield); cout.precision(10); cout.setf(ios::fixed,ios::floatfield);
+
+int main()
+{
+    optimize();
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        string s;
+        cin>>s;
+
+        for(int i=0; i<s.size(); i++)
+        {
+            if(s[i]=='1')
+            {
+                cout<<"13"<<endl;
+                break;
+            }
+            else if(s[i]=='3')
+            {
+                cout<<"31"<<endl;
+                break;
+            }
+        }
+    }
+}
