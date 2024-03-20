6caa440d752a9997f4d9ee0f361fb45a7010b281
Md.Sakib Chowdhury <136784151+csakib049@users.noreply.github.com>
2024-03-20 15:31:15 +0600

    Create Digits Summation.cpp

---
+++ Code Forces/Digits Summation.cpp
@@ -0,0 +1,27 @@
+//https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/F
+
+//================================================================================\\
+//===========================( اللهم صّلِ وسَلّمْ عَلۓِ نَبِيْنَا مُحَمد ﷺ )=========================\\
+//================================================================================\\
+ 
+ 
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#define endl "\n"
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define fraction() cout.unsetf(ios::floatfield); cout.precision(10); cout.setf(ios::fixed,ios::floatfield);
+ 
+int main()
+{
+    optimize();
+   
+  string s1,s2; cin>>s1>>s2;
+  
+  int a=s1[s1.size()-1]-'0';
+  int b=s2[s2.size()-1]-'0';
+
+cout<<a+b<<endl;   
+ 
+}
+ 
