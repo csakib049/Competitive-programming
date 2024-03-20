455caad1acda790065c8d9c27c540be678e93505
Md.Sakib Chowdhury <136784151+csakib049@users.noreply.github.com>
2024-03-20 15:21:25 +0600

    Create Area of a Circle.cpp

---
+++ Code Forces/Area of a Circle.cpp
@@ -0,0 +1,25 @@
+//https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/D
+
+//================================================================================\\
+//===========================( اللهم صّلِ وسَلّمْ عَلۓِ نَبِيْنَا مُحَمد ﷺ )=========================\\
+//================================================================================\\
+ 
+ 
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#define endl "\n"
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define fraction() cout.unsetf(ios::floatfield); cout.precision(10); cout.setf(ios::fixed,ios::floatfield);
+ 
+int main()
+{
+    optimize();
+   
+   double pie=3.141592653,R;  cin>>R;
+   
+   cout<< fixed << setprecision(9)<<pie*R*R<<endl;
+   
+ 
+}
+ 
