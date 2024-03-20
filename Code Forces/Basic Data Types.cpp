10752b02a0ae99703284fc90cf39fa7c287bf87d
Md.Sakib Chowdhury <136784151+csakib049@users.noreply.github.com>
2024-03-20 15:00:26 +0600

    Create Basic Data Types.cpp

---
+++ Code Forces/Basic Data Types.cpp
@@ -0,0 +1,29 @@
+//https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/B
+
+//================================================================================\\
+//===========================( اللهم صّلِ وسَلّمْ عَلۓِ نَبِيْنَا مُحَمد ﷺ )=========================\\
+//================================================================================\\
+ 
+ 
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#define endl "\n"
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define fraction() cout.unsetf(ios::floatfield); cout.precision(10); cout.setf(ios::fixed,ios::floatfield);
+ 
+int main()
+{
+    optimize();
+   string a,b,c,d,e; cin>>a>>b>>c>>d>>e;
+   
+   cout<<a<<endl;
+   cout<<b<<endl;
+   cout<<c<<endl;
+   cout<<d<<endl;
+   cout<<e<<endl;
+  
+   
+ 
+}
+ 
