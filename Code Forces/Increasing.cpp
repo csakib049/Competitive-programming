28edf930285c997e6272ba61668c7a4840fafda4
Md.Sakib Chowdhury <136784151+csakib049@users.noreply.github.com>
2024-03-24 09:24:24 +0600

    Update Increasing.cpp

--- Code Forces/Increasing.cpp
+++ Code Forces/Increasing.cpp
@@ -1,45 +1,34 @@
 //https://codeforces.com/contest/1742/problem/B
 
-
 //================================================================================\\
 //===========================( اللهم صّلِ وسَلّمْ عَلۓِ نَبِيْنَا مُحَمد ﷺ )=========================\\
 //================================================================================\\
-
-
+ 
+ 
 #include<bits/stdc++.h>
 using namespace std;
-
+ 
 #define endl "\n"
 #define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
 #define fraction() cout.unsetf(ios::floatfield); cout.precision(10); cout.setf(ios::fixed,ios::floatfield);
-
+ 
+ 
 int main()
 {
-    optimize();
+  optimize();
+ int t; cin>>t;
+ while(t--){
+   int n ;cin>>n;
+   vector<int>v(n);
+   for(int i=0;i<n;i++)cin>>v[i];
+   
+   sort(v.begin(),v.end());
+   int SZ=unique(v.begin(),v.end())-v.begin();
+   
+   if(SZ==n)cout<<"YES"<<endl;
+   else cout<<"NO"<<endl;
+   
 
-    int t;
-    cin>>t;
-    while(t--)
-    {
-
-
-        set<int> mySet;
-
-    int numElements;
-
-    cin >> numElements;
-
-
-    for (int i = 0; i < numElements; ++i) {
-        int inputValue;
-        cin >> inputValue;
-        mySet.insert(inputValue);
-    }
-
-      cout<<( numElements == mySet.size() ? "YES" : "NO" )<<endl;
-
-
-
-    }
+ }
 
 }
