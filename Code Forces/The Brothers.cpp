4a4a577df8921bf7e1eb35a808c15792be26888a
Md.Sakib Chowdhury <136784151+csakib049@users.noreply.github.com>
2024-03-27 13:45:14 +0600

    Create The Brothers.cpp

---
+++ Code Forces/The Brothers.cpp
@@ -0,0 +1,24 @@
+    //https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/L
+   
+    //================================================================================\\
+    //===========================( اللهم صّلِ وسَلّمْ عَلۓِ نَبِيْنَا مُحَمد ﷺ )=========================\\
+    //================================================================================\\
+     
+     
+    #include<bits/stdc++.h>
+    using namespace std;
+     
+    #define endl "\n"
+    #define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    #define fraction() cout.unsetf(ios::floatfield); cout.precision(10); cout.setf(ios::fixed,ios::floatfield);
+     
+    int main()
+    {
+        optimize();
+    string s1,s2; cin>>s1>>s2;
+    string s3,s4; cin>>s3>>s4;
+    
+    if(s2==s4)cout<<"ARE Brothers"<<endl;
+    else cout<<"NOT"<<endl;
+    
+    }
