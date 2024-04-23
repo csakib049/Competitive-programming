3432de7f41b45f5efb953ad8f9714d0b0c724087
Md.Sakib Chowdhury <136784151+csakib049@users.noreply.github.com>
2024-04-23 21:02:50 +0600

    Update Way Too Long Words.cpp

--- Code Forces/Way Too Long Words.cpp
+++ Code Forces/Way Too Long Words.cpp
@@ -1,25 +1,25 @@
-//https://codeforces.com/problemset/problem/71/A
-
+//https://codeforces.com/group/MWSDmqGsZm/contest/219856/problem/F
+ 
+//================================================================================\\
+//===========================( اللهم صّلِ وسَلّمْ عَلۓِ نَبِيْنَا مُحَمد ﷺ )=========================\\
+//================================================================================\\     
+ 
 #include<bits/stdc++.h>
 using namespace std;
-
+ 
+#define endl "\n"
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define fraction() cout.unsetf(ios::floatfield); cout.precision(10); cout.setf(ios::fixed,ios::floatfield);
+ 
+ 
 int main(){
-    int t;
-    cin>>t;
-    while(t--){
-
-     string s;
-     cin>>s;
-
-     if(s.size()>10){
-
-         cout<<s[0]<<s.size()-2<<s[s.size()-1]<<endl;
-
-     }else{
-
-         cout<<s<<endl;
-     }
-
-
-    }
+  int t; cin>>t;
+  
+  while(t--){
+    string s; cin>>s;
+    
+    if(s.size()<=10) cout<<s<<endl;
+    else cout<<s[0]<<s.size()-2<<s[s.size()-1]<<endl;
+  
+  }
 }
