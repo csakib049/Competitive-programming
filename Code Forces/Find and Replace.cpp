c8777deffd466aa96d94707ed2479ebeb6200c13
Md.Sakib Chowdhury <136784151+csakib049@users.noreply.github.com>
2024-05-01 18:11:00 +0600

    Create Find and Replace.cpp

---
+++ Code Forces/Find and Replace.cpp
@@ -0,0 +1,44 @@
+//https://codeforces.com/contest/1807/problem/C
+
+//================================================================================\\
+//===========================( اللهم صّلِ وسَلّمْ عَلۓِ نَبِيْنَا مُحَمد ﷺ )=========================\\
+//================================================================================\\     
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define endl "\n"
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define fraction() cout.unsetf(ios::floatfield); cout.precision(10); cout.setf(ios::fixed,ios::floatfield);
+
+int main(){
+
+  optimize();
+ int t; cin>>t;
+ while(t--){
+   int n; cin>>n;
+   string s; cin>>s;
+   
+   bool ok=true;
+   map<char,int>last;
+   
+   
+   for(int i=0;i<n;i++){
+     if(last.find(s[i])==last.end()){
+       last[s[i]]=i;
+     }else{
+       if((i-last[s[i]])%2!=0){
+         ok=false;
+         break;
+       }
+       last[s[i]]=i;
+     }
+   }
+   
+   if(ok==1)cout<<"YES"<<endl;
+   else cout<<"NO"<<endl;
+   
+
+ }
+ 
+}
