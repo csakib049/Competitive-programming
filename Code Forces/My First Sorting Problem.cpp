6e7785643c732e5be4dec89be0287e7aa09d35cf
Md.Sakib Chowdhury <136784151+csakib049@users.noreply.github.com>
2024-05-10 23:20:34 +0600

    Create My First Sorting Problem.cpp

---
+++ Code Forces/My First Sorting Problem.cpp
@@ -0,0 +1,25 @@
+//https://codeforces.com/contest/1971/problem/A
+   
+//================================================================================\\
+//===========================( اللهم صّلِ وسَلّمْ عَلۓِ نَبِيْنَا مُحَمد ﷺ )=========================\\
+//================================================================================\\
+     
+     
+#include<bits/stdc++.h>
+  using namespace std;
+     
+#define endl "\n"
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define fraction() cout.unsetf(ios::floatfield); cout.precision(10); cout.setf(ios::fixed,ios::floatfield);
+     
+int main()
+{
+   optimize();
+   int t; cin>>t;
+   while(t--){
+     int a,b; cin>>a>>b;
+     
+     cout<<min(a,b)<<" "<<max(a,b)<<endl;
+     
+   }
+ }
