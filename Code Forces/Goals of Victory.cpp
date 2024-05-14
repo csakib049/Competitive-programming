48340bb0497dee256d03d9e960c2deffffb9e0cd
Md.Sakib Chowdhury <136784151+csakib049@users.noreply.github.com>
2024-05-14 08:05:34 +0600

    Create Goals of Victory.cpp

---
+++ Code Forces/Goals of Victory.cpp
@@ -0,0 +1,31 @@
+//https://codeforces.com/problemset/problem/1877/A
+   
+//================================================================================\\
+//===========================( اللهم صّلِ وسَلّمْ عَلۓِ نَبِيْنَا مُحَمد ﷺ )=========================\\
+//================================================================================\\
+     
+     
+#include<bits/stdc++.h>
+using namespace std;
+     
+#define endl "\n"
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define fraction() cout.unsetf(ios::floatfield); cout.precision(10); cout.setf(ios::fixed,ios::floatfield);
+     
+int main()
+{
+    optimize();
+       int t; cin>>t;
+       while(t--){
+         int n; cin>>n;
+        vector<int>v(n-1);
+        for(int i=0;i<n-1;i++)cin>>v[i];
+        
+        int ans=0;
+        for(int i=0;i<n-1;i++){
+          ans=ans+v[i];
+        }
+         
+         cout<<(-1*ans)<<endl;
+       }
+ }
