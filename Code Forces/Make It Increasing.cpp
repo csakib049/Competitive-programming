e3f793ebba13c2d7f542ce0a7d1642310f321fa7
Md.Sakib Chowdhury <136784151+csakib049@users.noreply.github.com>
2024-08-24 14:33:08 +0900

    Create Make It Increasing.cpp

---
+++ Code Forces/Make It Increasing.cpp
@@ -0,0 +1,47 @@
+//https://codeforces.com/problemset/problem/1675/B
+
+//================================================================================\\
+//===========================( اللهم صّلِ وسَلّمْ عَلۓِ نَبِيْنَا مُحَمد ﷺ )=========================\\
+//================================================================================\\
+ 
+ 
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#define endl "\n"
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define fraction() cout.unsetf(ios::floatfield); cout.precision(10); cout.setf(ios::fixed,ios::floatfield);
+ 
+int main()
+{
+    optimize();
+   int t; cin>>t;
+   while(t--){
+    int n; cin>>n;
+    vector<int>v(n);
+    for(int i=0;i<n;i++)cin>>v[i];
+    
+    
+    int cnt=0;
+    for(int i=n-2;i>=0;i--){
+      
+      if(v[i+1]==0){
+        
+        cnt=-1;
+        break;
+        
+      }
+      
+      while( v[i]>=v[i+1]){
+        
+        v[i]=v[i]/2;
+        cnt++;
+      }
+    }
+    
+     cout<<cnt<<endl;
+     
+   }
+
+}
+
