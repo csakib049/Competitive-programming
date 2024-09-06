43cb0701a9bf154b85315d357ca3341aab82aa87
Md.Sakib Chowdhury <136784151+csakib049@users.noreply.github.com>
2024-09-07 00:31:01 +0900

    Create Odd Divisor.cpp

---
+++ Code Forces/Odd Divisor.cpp
@@ -0,0 +1,39 @@
+//https://codeforces.com/problemset/problem/1475/A
+
+//================================================================================\\
+//===========================( اللهم صّلِ وسَلّمْ عَلۓِ نَبِيْنَا مُحَمد ﷺ )=========================\\
+//================================================================================\\
+ 
+ 
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#define endl "\n"
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define fraction() cout.unsetf(ios::floatfield); cout.precision(10); cout.setf(ios::fixed,ios::floatfield);
+
+
+bool allelementsequal(const vector<int>& vec) {
+    if (vec.empty()) return true; // An empty vector is considered to have all equal elements
+    return std::all_of(vec.begin(), vec.end(), [first = vec[0]](int element) {
+        return element == first;
+    });
+}
+
+
+int main()
+{
+    optimize();
+   int t; cin>>t;
+   while(t--){
+   	
+   	long long n; cin>>n;
+   	
+   	if((n&(n-1))==0)cout<<"NO"<<endl;
+   	else cout<<"YES"<<endl;
+   	
+   }
+}
+
+//https://www.youtube.com/watch?v=2LlsfyG6IH8
+ 
