ed4acde985385f20cd45d1aa86e537b0d16e1095
Md.Sakib Chowdhury <csakib049@gmail.com>
2026-02-26 06:52:18 +0600

    solving mahasena

---
+++ CodeChef/Array/Mahasena.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+  
+  int n; cin>>n;
+  
+  int a[n]; for(int i=0;i<n;i++)cin>>a[i];
+  
+  int even=0; int odd=0;
+  
+  for(int i=0;i<n;i++){
+      if(a[i]%2==0)even++;
+      else odd++;
+  }
+  
+  
+  if(even>odd)cout<<"READY FOR BATTLE";
+  else cout<<"NOT READY";
+
+}
