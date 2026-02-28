ff27649756a34d341f0f2ae34429236bc4b05612
Md.Sakib Chowdhury <csakib049@gmail.com>
2026-02-28 15:00:20 +0600

    starting 800 problems in codechef

---
+++ CodeChef/800 rating /New_Year_Resolution.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+void solve(){
+
+    int a; cin>>a;
+    
+    cout<<a*31<<endl;
+
+}
+
+
+int main(){
+
+        
+        solve();
+
+
+    
+
+    return 0;
+}
