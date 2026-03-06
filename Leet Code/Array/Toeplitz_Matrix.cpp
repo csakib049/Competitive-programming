16a6b976d59af47e82f2bab4a2150ca119441e76
Md.Sakib Chowdhury <136784151+csakib049@users.noreply.github.com>
2026-03-06 09:06:43 +0600

    Optimize isToeplitzMatrix function implementation
    Refactored isToeplitzMatrix function for optimization.

--- Leet Code/Array/Toeplitz_Matrix.cpp
+++ Leet Code/Array/Toeplitz_Matrix.cpp
@@ -1,4 +1,25 @@
 class Solution {
+
+public:
+    bool isToeplitzMatrix(vector<vector<int>>& matrix) {
+ 
+
+        int height=matrix.size();
+        int weidth=matrix[0].size();
+
+        for(int i=1;i<height;i++){
+            for(int j=1;j<weidth;j++){
+               if(matrix[i][j]!=matrix[i-1][j-1]) return false;
+            }
+        }
+
+        return true;
+    }
+};
+//------------------------------------------------------
+//optimized 
+
+class Solution {
 public:
     bool isToeplitzMatrix(vector<vector<int>>& matrix) {
 
