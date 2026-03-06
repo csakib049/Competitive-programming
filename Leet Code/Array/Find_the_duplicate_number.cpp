85278b855848f36ddbc6980fab1cee09cbe0b6d3
Md.Sakib Chowdhury <136784151+csakib049@users.noreply.github.com>
2026-03-06 09:02:50 +0600

    Implement optimized duplicate finder using unordered_set
    Added an optimized solution using unordered_set to find duplicates.

--- Leet Code/Array/Find_the_duplicate_number.cpp
+++ Leet Code/Array/Find_the_duplicate_number.cpp
@@ -14,3 +14,22 @@ public:
     }
 
 };
+
+//--------------------------------------------------
+//optimized
+
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        
+        unordered_set<int>seen;
+        for(auto num:nums){
+            if(seen.count(num))return num;
+            else seen.insert(num);
+        }
+
+
+        return -1;
+    }
+
+};
