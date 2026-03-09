5f7ddc069b770331d2d5be7ef15d789b52e30491
Md.Sakib Chowdhury <csakib049@gmail.com>
2026-03-09 19:22:11 +0600

    Spiral Matrix

---
+++ Leet Code/Array/Spiral_Matrix.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        int height=matrix.size();
+        int weidth=matrix[0].size();
+
+        int left=0;
+        int right=weidth-1;
+        int top=0;
+        int bottom=height-1;
+
+        vector<int>ans;
+
+        while(left<=right && top<=bottom){
+
+            for(int i=left;i<=right;i++){
+               ans.push_back(matrix[top][i]);
+            }
+            top++;
+
+            for(int i=top;i<=bottom;i++){
+               ans.push_back(matrix[i][right]);
+            }
+            right--;
+
+               
+               if(!(left<=right && top<=bottom))break;
+
+              
+            for(int i=right;i>=left;i--){
+               ans.push_back(matrix[bottom][i]);
+            }
+            bottom--;
+
+            for(int i=bottom;i>=top;i--){
+               ans.push_back(matrix[i][left]);
+            }
+            left++;
+        }
+
+        
+        return ans;
+    }
+};
