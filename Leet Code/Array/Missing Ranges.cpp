31504470917cb6e4b368234e0eb1282bd51ebaf8
Md.Sakib Chowdhury <csakib049@gmail.com>
2026-03-10 15:40:03 +0600

    Optimized answer of Missing Ranges

--- Leet Code/Array/Missing Ranges.cpp
+++ Leet Code/Array/Missing Ranges.cpp
@@ -1,3 +1,5 @@
+//    https://drive.google.com/file/d/1KiV2MPjYbcGAzng9QR4ElgAjq8s-U_fz/view?pli=1
+
 class solution{
     public:
       
@@ -28,6 +30,31 @@ class solution{
         }
         if(x==-1)ans.push_back({x,upper});
 
+        return ans;
+     }
+
+}
+
+//-----------------------------------------------------------------
+//optimized
+
+
+class solution{
+    public:
+
+     vector<vector<int>> findMissingRanges(vector<int>& nums, int lower, int upper){
+           
+        vector<vector<int>> ans; 
+           
+        nums.insert(nums.begin(),lower-1);  // insert lower-1 value in the first 
+        nums.push_back(upper+1);  // push upper+1 value in the last 
+
+        for(int i=0;i<nums.size()-1;i++){  //O(n)
+
+            if((nums[i+1]-nums[i])>1){
+                ans.push_back({nums[i]+1,nums[i+1]-1});    // if the array is [1,2,3,9] ==> [4,8] 
+            }
+        }
         return ans;
      }
 
